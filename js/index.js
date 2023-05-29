e4ed414eb3b1daa205351418ce5710385e0c52e4
Stanley <ohiaeristanley124@gmail.com>
2023-05-29 19:01:42 +0100

    version update

--- js/index.js
+++ js/index.js
@@ -7,4 +7,49 @@ document.querySelector('.call').addEventListener('click', function() {
       loader.style.display = 'none'; // Hide the loader
     }, 2000);
   });
-  
+// Get the call button element
+var callButton = document.querySelector('.call');
+
+// Add click event listener to the call button
+callButton.addEventListener('click', function() {
+  // Call the changeButtonColor() function when the button is clicked
+  changeButtonColor();
+});
+// Function to change the button color
+function changeButtonColor() {
+  // Add the "clicked" class to the call button
+  callButton.classList.add('clicked');
+}
+
+
+// Get the call button element
+var mapButton = document.querySelector('.mapButton');
+
+// Add click event listener to the call button
+mapButton.addEventListener('click', function() {
+  // Call the changeButtonColor() function when the button is clicked
+  changemapButtonColor();
+});
+
+// Function to change the button color
+function changemapButtonColor() {
+  // Add the "clicked" class to the call button
+  mapButton.classList.add('clicked');
+}
+
+
+// Get the call button element
+var websiteButton = document.querySelector('.websiteButton');
+
+// Add click event listener to the call button
+websiteButton.addEventListener('click', function() {
+  // Call the changeButtonColor() function when the button is clicked
+  changewebsiteButtonColor();
+});
+
+// Function to change the button color
+function changewebsiteButtonColor() {
+  // Add the "clicked" class to the call button
+  websiteButton.classList.add('clicked');
+}
+
